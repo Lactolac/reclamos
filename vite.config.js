c03baa8ebab24d273fecc7de11d8432052d32c74
samdev <samuantonio14@gmail.com>
2025-03-14 18:26:11 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias para importar archivos desde la carpeta src
+    },
+  },
   server: {
-    host: '0.0.0.0',
-    port: 3030, // Puedes cambiar el puerto si lo deseas
-  }
+    host: '0.0.0.0', // Escucha en todas las interfaces de red
+    port: 5173, // Puedes cambiar el puerto si es necesario
+    allowedHosts: [
+      'zdownload.yes.com.sv' // Permitir solicitudes desde este dominio
+    ]
+  },
 })
